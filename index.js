8e041274ff3014542cf99b5e38384cb7926bec65
tereneka <teren-eka@yandex.ru>
2022-11-16 00:20:47 +0300

    fix: fix the project viewing mode for mobile

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 // navigation
-const navElem = document.querySelector(".nav");
-const navIconElems = navElem.querySelectorAll(".nav__icon");
-const blockElems = document.querySelectorAll(".block");
+const navElemen = document.querySelector(".nav");
+const navIconElements = navElemen.querySelectorAll(".nav__icon");
+const blockElements = document.querySelectorAll(".block");
 
 function toggleNav() {
-  blockElems.forEach((block, index) => {
+  blockElements.forEach((block, index) => {
     let position = block.getBoundingClientRect();
     if (position.top <= 0 && position.bottom > 0) {
-      navIconElems[index].classList.add("nav__icon_active");
+      navIconElements[index].classList.add("nav__icon_active");
     } else {
-      navIconElems[index].classList.remove("nav__icon_active");
+      navIconElements[index].classList.remove("nav__icon_active");
     }
   });
 }
 
 document.addEventListener("scroll", toggleNav);
+
+// просмотр проектов для мобильных устройств
+const projectsElement = document.querySelector(".projects");
+const projectsListElements =
+  projectsElement.querySelectorAll(".projects__item");
+const projectsOverlayElements =
+  projectsElement.querySelectorAll(".projects__overlay");
+
+projectsListElements.forEach((project, index) => {
+  project.addEventListener("click", () => {
+    projectsOverlayElements[index].classList.toggle(
+      "projects__overlay_visible"
+    );
+  });
+});
+
+document.addEventListener("click", (e) => {
+  projectsOverlayElements.forEach((overlay) => {
+    if (e.target != overlay) {
+      overlay.classList.remove("projects__overlay_visible");
+    }
+  });
+});
